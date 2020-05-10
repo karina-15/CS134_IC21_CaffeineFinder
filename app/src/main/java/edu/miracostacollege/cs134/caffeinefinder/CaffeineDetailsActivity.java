6482f892b9620ad44a2cfe4715306c7dd129242d
Karina Elias <karinaelias55@gmail.com>
2020-05-10 14:31:22 -0700

    Initial commit Need to complete CaffeineDetailsActivity

---
+++ app/src/main/java/edu/miracostacollege/cs134/caffeinefinder/CaffeineDetailsActivity.java
@@ -0,0 +1,14 @@
+package edu.miracostacollege.cs134.caffeinefinder;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CaffeineDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_caffeine_details);
+    }
+}
